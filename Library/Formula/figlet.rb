1c30e8ea5bf1bb29699efe8257a8ada176fe4afd
Mel Gray <melgray@gmail.com>
2009-10-08 18:55:08 -0700

    Actually looked at the Makefile for figlet and it was a bit wonky.  Copying things to places where I didn't want them.  This keeps everything in its prefix

--- Library/Formula/figlet.rb
+++ Library/Formula/figlet.rb
@@ -5,13 +5,22 @@ class Figlet <Formula
   homepage 'http://www.figlet.org/'
   md5 '5f84ad52d092d5db4ad5916df767646b'
 
-  def font_dir
-    [HOMEBREW_PREFIX, "share", "fonts"].join("/")
+  def destination_dir
+    [prefix, 'bin'].join("/")    
   end
 
+  def man_dir
+    [prefix, "man"].join("/")    
+  end
+
+  def font_dir
+    [prefix, "share", "fonts"].join("/")
+  end
+   
   def install
-    system "make figlet"
-    system "make install DESTDIR=#{prefix} DEFAULTFONTDIR=#{font_dir}"
+    system "make figlet DEFAULTFONTDIR=#{font_dir}"
+    system "mkdir #{destination_dir}"
+    system "make install DESTDIR=#{destination_dir} DEFAULTFONTDIR=#{font_dir} MANDIR=#{man_dir}"
   end
   
 end
